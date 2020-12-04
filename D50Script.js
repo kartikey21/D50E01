f86a6f116abd478ce03d244a861246b3fde216a2
kartikey21 <kartikey.jain.21@outlook.com>
2020-12-04 10:12:48 +0530

    Final Commit

--- D50Script.js
+++ D50Script.js
@@ -1,1 +1,21 @@
-﻿
+﻿function allowDropThis(i) {
+
+    i.preventDefault();
+
+}
+
+function dragThis(i) {
+
+    i.dataTransfer.setData("obj", i.target.id);
+
+}
+
+function dropThis(i) {
+
+    i.preventDefault();
+
+    var data = i.dataTransfer.getData("obj");
+
+    i.target.appendChild(document.getElementById(data));
+
+}
